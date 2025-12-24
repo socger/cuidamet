0d282ef421ae8d74a7d1dd8aa10719b5bf144c6e
Jero <socger@gmail.com>
2025-12-24 12:26:41 +0100

    feat: add allowedHosts configuration in Vite server settings

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 5173,
         host: '0.0.0.0',
+        allowedHosts: ['cuidamet.duckdns.org', 'localhost'],
       },
       plugins: [react()],
       define: {
